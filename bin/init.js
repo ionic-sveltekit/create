13364f99fe2c50fc89c5041a0b299de103c1a300
Brad Mallow <briznad@gmail.com>
2025-03-21 12:41:29 +0000

    use import vs require

--- bin/init.js
+++ bin/init.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+import fs from 'fs-extra';
+import path from 'path';
+import { execSync } from 'child_process';
 
 // Check if node_modules exists in the package directory
 const packageDir = path.resolve(__dirname, '..');
